743bbdfda595d7888910848e3ee63ed97695aeb4
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-06-14 10:07:59 +0200

    Texturation Cylindre en test

--- Assets/scripts/TextureCylinder.cs
+++ Assets/scripts/TextureCylinder.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TextureCylinder : MonoBehaviour {
+	private int i;
+
 	void Start () {
 		MeshFilter mf = GetComponent<MeshFilter> ();
 		Mesh mesh = mf != null ? mf.mesh : null;
@@ -12,98 +14,115 @@ public class TextureCylinder : MonoBehaviour {
 			return;
 		}
 
+		float[,] colors = new float[,] {
+			// Side bottom points
+			{ 0.0f, 1.0f },	//  0
+			{ 1.0f, 1.0f },	//  1
+			{ 2.0f, 1.0f },	//  2
+			{ 3.0f, 1.0f },	//  3
+			{ 4.0f, 1.0f },	//  4
+			{ 5.0f, 1.0f },	//  5
+			{ 6.0f, 1.0f },	//  6
+			{ 7.0f, 1.0f },	//  7
+			{ 8.0f, 1.0f },	//  8
+			{ 9.0f, 1.0f },	//  9
+			{10.0f, 1.0f },	// 10
+			{11.0f, 1.0f },	// 11
+			{12.0f, 1.0f },	// 12
+			{13.0f, 1.0f },	// 13
+			{14.0f, 1.0f },	// 14
+			{15.0f, 1.0f },	// 15
+			{16.0f, 1.0f },	// 16
+			{17.0f, 1.0f },	// 17
+			{18.0f, 1.0f },	// 18
+			{19.0f, 1.0f },	// 19
+
+			// Side top points
+			{ 0.0f, 3.0f },	// 20
+			{ 1.0f, 3.0f },	// 21
+			{ 2.0f, 3.0f },	// 22
+			{ 3.0f, 3.0f },	// 23
+			{ 4.0f, 3.0f },	// 24
+			{ 5.0f, 3.0f },	// 25
+			{ 6.0f, 3.0f },	// 26
+			{ 7.0f, 3.0f },	// 27
+			{ 8.0f, 3.0f },	// 28
+			{ 9.0f, 3.0f },	// 29
+			{10.0f, 3.0f },	// 30
+			{11.0f, 3.0f },	// 31
+			{12.0f, 3.0f },	// 32
+			{13.0f, 3.0f },	// 33
+			{14.0f, 3.0f },	// 34
+			{15.0f, 3.0f },	// 35
+			{16.0f, 3.0f },	// 36
+			{17.0f, 3.0f },	// 37
+			{18.0f, 3.0f },	// 38
+			{19.0f, 3.0f },	// 39
+
+			// Center
+			{10.0f, 0.0f },	// 40	// Bottom
+			{10.0f, 4.0f },	// 41	// Top
+
+			// Missed first band 
+			{ 9.0f, 1.0f },	// 42	// Bottom	theta = 0
+			{ 9.0f, 3.0f },	// 43	// Top 		theta = 0
+
+			// Missed second band
+			{19.0f, 1.0f },	// 44	// Bottom	theta = pi
+			{19.0f, 3.0f },	// 45	// Top 		theta = pi
+			{20.0f, 3.0f },	// 46	// Top		theta = -9pi/10
+			{20.0f, 1.0f },	// 47	// Bottom	theta = -9pi/10
+
+			// Bottom circle
+			{ 1.0f, 1.0f },	// 48
+			{ 0.0f, 1.0f },	// 49
+			{ 2.0f, 1.0f },	// 50
+			{ 3.0f, 1.0f },	// 51
+			{ 4.0f, 1.0f },	// 52
+			{ 5.0f, 1.0f },	// 53
+			{ 6.0f, 1.0f },	// 54
+			{ 7.0f, 1.0f },	// 55
+			{ 8.0f, 1.0f },	// 56
+			{ 9.0f, 1.0f },	// 57
+			{10.0f, 1.0f },	// 58
+			{11.0f, 1.0f },	// 59
+			{12.0f, 1.0f },	// 60
+			{13.0f, 1.0f },	// 61
+			{14.0f, 1.0f },	// 62
+			{15.0f, 1.0f },	// 63
+			{16.0f, 1.0f },	// 64
+			{17.0f, 1.0f },	// 65
+			{18.0f, 1.0f },	// 66
+			{19.0f, 1.0f },	// 67
+
+			// Top circle
+			{ 0.0f, 3.0f },	// 68
+			{ 1.0f, 3.0f },	// 69
+			{ 2.0f, 3.0f },	// 70
+			{ 3.0f, 3.0f },	// 71
+			{ 4.0f, 3.0f },	// 72
+			{ 5.0f, 3.0f },	// 73
+			{ 6.0f, 3.0f },	// 74
+			{ 7.0f, 3.0f },	// 75
+			{ 8.0f, 3.0f },	// 76
+			{ 9.0f, 3.0f },	// 77
+			{10.0f, 3.0f },	// 78
+			{11.0f, 3.0f },	// 79
+			{12.0f, 3.0f },	// 80
+			{13.0f, 3.0f },	// 81
+			{14.0f, 3.0f },	// 82
+			{15.0f, 3.0f },	// 83
+			{16.0f, 3.0f },	// 84
+			{17.0f, 3.0f },	// 85
+			{18.0f, 3.0f },	// 86
+			{19.0f, 3.0f }	// 87
+		};
+
 		Vector2[] uvs = new Vector2[mesh.vertices.Length];
 
-		uvs[40]	= new Vector2(0.000f, 0.666f);
-		uvs[48]	= new Vector2(0.000f, 0.334f);
-		// uvs[0]	= new Vector2(0.000f, 0.000f);
-		// uvs[1]	= new Vector2(0.000f, 0.000f);
-		// uvs[2]	= new Vector2(1.000f, 0.333f);
-		// uvs[3]	= new Vector2(0.000f, 0.333f);
-		// uvs[4]	= new Vector2(0.000f, 0.000f);
-		// uvs[5]	= new Vector2(0.000f, 0.000f);
-		// uvs[6]	= new Vector2(0.000f, 0.000f);
-		// uvs[7]	= new Vector2(0.000f, 0.000f);
-		// uvs[8]	= new Vector2(0.000f, 0.000f);
-		// uvs[9]	= new Vector2(0.000f, 0.000f);
-		// uvs[10]	= new Vector2(0.000f, 0.000f);
-		// uvs[11]	= new Vector2(0.000f, 0.000f);
-		// uvs[12]	= new Vector2(0.000f, 0.000f);
-		// uvs[13]	= new Vector2(0.000f, 0.000f);
-		// uvs[14]	= new Vector2(0.000f, 0.000f);
-		// uvs[15]	= new Vector2(0.000f, 0.000f);
-		// uvs[16]	= new Vector2(0.000f, 0.000f);
-		// uvs[17]	= new Vector2(0.000f, 0.000f);
-		// uvs[18]	= new Vector2(0.000f, 0.000f);
-		// uvs[19]	= new Vector2(0.000f, 0.000f);
-		// uvs[20]	= new Vector2(0.000f, 0.000f);
-		// uvs[21]	= new Vector2(0.000f, 0.000f);
-		// uvs[22]	= new Vector2(0.000f, 0.000f);
-		// uvs[23]	= new Vector2(0.000f, 0.000f);
-		// uvs[24]	= new Vector2(0.000f, 0.000f);
-		// uvs[25]	= new Vector2(0.000f, 0.000f);
-		// uvs[26]	= new Vector2(0.000f, 0.000f);
-		// uvs[27]	= new Vector2(0.000f, 0.000f);
-		// uvs[28]	= new Vector2(0.000f, 0.000f);
-		// uvs[29]	= new Vector2(0.000f, 0.000f);
-		// uvs[30]	= new Vector2(0.000f, 0.000f);
-		// uvs[31]	= new Vector2(0.000f, 0.000f);
-		// uvs[32]	= new Vector2(0.000f, 0.000f);
-		// uvs[33]	= new Vector2(0.000f, 0.000f);
-		// uvs[34]	= new Vector2(0.000f, 0.000f);
-		// uvs[35]	= new Vector2(0.000f, 0.000f);
-		// uvs[36]	= new Vector2(0.000f, 0.000f);
-		// uvs[37]	= new Vector2(0.000f, 0.000f);
-		// uvs[38]	= new Vector2(0.000f, 0.000f);
-		// uvs[39]	= new Vector2(0.000f, 0.000f);
-		// uvs[40]	= new Vector2(0.000f, 0.000f);
-		// uvs[41]	= new Vector2(0.000f, 0.000f);
-		// uvs[42]	= new Vector2(0.000f, 0.000f);
-		// uvs[43]	= new Vector2(0.000f, 0.000f);
-		// uvs[44]	= new Vector2(0.000f, 0.000f);
-		// uvs[45]	= new Vector2(0.000f, 0.000f);
-		// uvs[46]	= new Vector2(0.000f, 0.000f);
-		// uvs[47]	= new Vector2(0.000f, 0.000f);
-		// uvs[48]	= new Vector2(0.000f, 0.000f);
-		// uvs[49]	= new Vector2(0.000f, 0.000f);
-		// uvs[50]	= new Vector2(0.000f, 0.000f);
-		// uvs[51]	= new Vector2(0.000f, 0.000f);
-		// uvs[52]	= new Vector2(0.000f, 0.000f);
-		// uvs[53]	= new Vector2(0.000f, 0.000f);
-		// uvs[54]	= new Vector2(0.000f, 0.000f);
-		// uvs[55]	= new Vector2(0.000f, 0.000f);
-		// uvs[56]	= new Vector2(0.000f, 0.000f);
-		// uvs[57]	= new Vector2(0.000f, 0.000f);
-		// uvs[58]	= new Vector2(0.000f, 0.000f);
-		// uvs[59]	= new Vector2(0.000f, 0.000f);
-		// uvs[60]	= new Vector2(0.000f, 0.000f);
-		// uvs[61]	= new Vector2(0.000f, 0.000f);
-		// uvs[62]	= new Vector2(0.000f, 0.000f);
-		// uvs[63]	= new Vector2(0.000f, 0.000f);
-		// uvs[64]	= new Vector2(0.000f, 0.000f);
-		// uvs[65]	= new Vector2(0.000f, 0.000f);
-		// uvs[66]	= new Vector2(0.000f, 0.000f);
-		// uvs[67]	= new Vector2(0.000f, 0.000f);
-		// uvs[68]	= new Vector2(0.000f, 0.000f);
-		// uvs[69]	= new Vector2(0.000f, 0.000f);
-		// uvs[70]	= new Vector2(0.000f, 0.000f);
-		// uvs[71]	= new Vector2(0.000f, 0.000f);
-		// uvs[72]	= new Vector2(0.000f, 0.000f);
-		// uvs[73]	= new Vector2(0.000f, 0.000f);
-		// uvs[74]	= new Vector2(0.000f, 0.000f);
-		// uvs[75]	= new Vector2(0.000f, 0.000f);
-		// uvs[76]	= new Vector2(0.000f, 0.000f);
-		// uvs[77]	= new Vector2(0.000f, 0.000f);
-		// uvs[78]	= new Vector2(0.000f, 0.000f);
-		// uvs[79]	= new Vector2(0.000f, 0.000f);
-		// uvs[80]	= new Vector2(0.000f, 0.000f);
-		// uvs[81]	= new Vector2(0.000f, 0.000f);
-		// uvs[82]	= new Vector2(0.000f, 0.000f);
-		// uvs[83]	= new Vector2(0.000f, 0.000f);
-		// uvs[84]	= new Vector2(0.000f, 0.000f);
-		// uvs[85]	= new Vector2(0.000f, 0.000f);
-		// uvs[86]	= new Vector2(0.000f, 0.000f);
-		// uvs[87]	= new Vector2(0.000f, 0.000f);
+		for (int i = 0; i < mesh.vertices.Length; ++i) {
+			uvs [i] = new Vector2 (colors[i, 0] / 20.0f, colors[i, 1] / 4.0f);
+		}
 
 		mesh.uv = uvs;
 	}
