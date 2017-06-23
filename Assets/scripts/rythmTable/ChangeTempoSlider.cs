5f7218cae386d989e3bbb89dde03ff4cdafeb787
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-06-23 14:17:24 +0200

    Correction Warnings

--- Assets/scripts/rythmTable/ChangeTempoSlider.cs
+++ Assets/scripts/rythmTable/ChangeTempoSlider.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 
 public class ChangeTempoSlider : Effect
 {
-    private float initValue = 0.5f;
-    private float actualGain;
-
+	public GameObject rythmtable;
+	public TextMesh text;
     public float actualValue { get; set; }
     public float faderValue { get; set; }
 
-	public TextMesh text;
-
-
-    public GameObject rythmtable;
-
-    void Start()
-    {
-     
-    }
-
-    void Update()
-    {
-    }
-
     public override void ApplyEffect(float value)
     {
-		value = value + 0.2f;
+		value += 0.2f;
         if (value != -1) { actualValue = value; }
         rythmtable.GetComponent<RythmTable>().changeTempo(actualValue* 0.5f);
 		text.text = (int)(1/actualValue*60) + "BPM";
