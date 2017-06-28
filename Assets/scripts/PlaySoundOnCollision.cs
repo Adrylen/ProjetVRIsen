6acbf14107e0a9bfe7b57b90c5cbf706e5c576a0
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-06-28 14:21:00 +0200

    Correction finale batterie

--- Assets/scripts/PlaySoundOnCollision.cs
+++ Assets/scripts/PlaySoundOnCollision.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlaySoundOnCollision : MonoBehaviour
 {
     private AudioSource sound;
-    
-	public string filename;
+    private SteamVR_TrackedController controller;
+
+    public string filename;
+
     // Use this for initialization
     void Start()
     {
@@ -17,17 +19,23 @@ public class PlaySoundOnCollision : MonoBehaviour
         }
     }
 
+    void Vibration()
+    {
+        SteamVR_Controller.Input((int)controller.controllerIndex).TriggerHapticPulse((ushort)3999);
+    }
+
+
     // Mettre un collider sur chacun des objets
     void OnTriggerEnter(Collider test)
     {
-        SteamVR_TrackedController controller = test.GetComponentInParent<SteamVR_TrackedController>();
+        controller = test.GetComponentInParent<SteamVR_TrackedController>();
         
         if (test.gameObject.CompareTag("Pickable"))
         {
             GetComponent<AudioSource>().PlayOneShot(LoadResources.soundFiles[filename]);
             if (controller != null)
             {
-                SteamVR_Controller.Input((int)controller.controllerIndex).TriggerHapticPulse((ushort)3999);
+                Invoke("Vibration", 0.2f);
             }
         }
     }
